8d7c6497382ddafc08e5cc9d6c7839c835ef90c1
lbmello <lucasbmello96@gmail.com>
2019-04-15 21:36:53 -0300

    Importado o módulo copy e executada a função.

--- synchronize.py
+++ synchronize.py
@@ -1,1 +1,4 @@
 # Arquivo principal do projeto onde as demais funções são executadas em sequência.
+from lib import copy as cp
+
+cp.copy_file('/home/share/sync/*','vagrant','192.168.15.10','/tmp/rsync/')
