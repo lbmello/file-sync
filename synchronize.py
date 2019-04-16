4e5ba5070e513909232cce501c3b870547fc74e1
lbmello <lucasbmello96@gmail.com>
2019-04-15 22:56:16 -0300

    Lidos todos os dados em JSON.

--- synchronize.py
+++ synchronize.py
@@ -1,4 +1,17 @@
 # Arquivo principal do projeto onde as demais funções são executadas em sequência.
 from lib import copy as cp
+from lib import json_reader as jreader
 
-cp.copy_file('/home/share/sync/*','vagrant','192.168.15.10','/tmp/rsync/')
+# READ THE TIME CONFIG
+time_default, time_lab = jreader.json_time_read()
+
+# READ THE GLOBAL CONFIG
+global_user, global_time = jreader.json_global_read()
+
+# READ THE HOSTS CONFIG
+json_nodes, json_level0, json_level1, json_level2, nodes, level0, level1, level2 = jreader.json_hosts_read()
+
+# READ THE SHARE CONFIG
+share_id, share_name, share_description, share_author, share_enviroment, share_sync_level, share_node, share_source, share_user, share_ip, share_destiny, share_time = jreader.json_config_read()
+
+cp.copy_file(share_source,share_user,share_ip,share_destiny)
