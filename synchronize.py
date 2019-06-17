1604c57d822327927f795c215d10bdae4f7a336c
lbmello <lucasbmello96@gmail.com>
2019-06-17 20:08:14 -0300

    Adicionado o return de tempo.

--- synchronize.py
+++ synchronize.py
@@ -14,6 +14,8 @@ json_nodes, json_level0, json_level1, json_level2, nodes, level0, level1, level2
 # READ THE SHARE CONFIG
 share_id, share_name, share_description, share_author, share_enviroment, share_sync_level, share_node, share_source, share_user, share_destiny, share_time = jreader.json_config_read()
 
+# READ THE TIME CONFIG
+time_default, time_lab = jreader.json_time_read()
 
 # RUN THE SYNC WITH IP ARRAY
 for i in range(len(ip)):
