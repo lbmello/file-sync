286c97de0a16f42fde5544e5220722eb638e92a1
lucas <lucasbmello96@gmail.com>
2020-06-04 22:04:34 -0300

    Tarefas de crontab criadas com main ajustado

--- filesync/internal/__init__.py
+++ filesync/internal/__init__.py
@@ -2,5 +2,23 @@
 
 """Classe irá gerenciar as interações com o SO, como leitura de dados em pastas."""
 
-from .Changes import Changes
-from .Tree import Tree
+from .changes import Changes
+from .tree import Tree
+from .ssh import Ssh
+from .cron import cron
+
+from ..enter import _time_objects_
+from ..enter import _config_objects_
+
+_cron_objs_ = list()
+
+for time in _time_objects_:
+    for config in _config_objects_:
+
+        if time.name == config.time:
+            # Instancia de Cron
+            cron_object = (cron( time_obj = time, user = config.user))
+
+            cron_object.set_crontab_all(command = '/bin/echo meu_ovo')
+
+            _cron_objs_.append(cron_object)
