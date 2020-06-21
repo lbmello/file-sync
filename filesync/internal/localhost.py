b314ab87f0ba947cd6e10a3b358d6f005940f68a
lucas <lucasbmello96@gmail.com>
2020-06-21 20:13:59 -0300

    add localhost em Hosts.json somente na primeira exec

--- filesync/internal/localhost.py
+++ filesync/internal/localhost.py
@@ -5,21 +5,48 @@ de internal.domain."""
 import socket
 
 from ..data.write import Host as write_host
+from ..enter import _node_objects_
 
 class localhost:
+    """ Classe responsavel pela criaçao do registro de localhost em Hosts.JSON."""
 
     def __init__(self):
         self.ip = socket.gethostbyname(socket.gethostname())
         self.name = 'localhost'
         self.description = 'maquina_local'
 
-        write_host_object = write_host()
-        write_host_object.set_host( name = self.name,
-                                    ip = self.ip,
-                                    description = self.description,
-                                    uid = '000',
-                                    edge = '')
-        print('localhost criado')
+        self._set_hostname()
+
+
+    def _set_hostname(self):
+        """ Percorre lista dos nodes e cria localhost, caso nao exista."""
+
+        if not _node_objects_:
+            write_host_object = write_host()
+            write_host_object.set_host( name = self.name,
+                                        ip = self.ip,
+                                        description = self.description,
+                                        uid = '000',
+                                        edge = '')
+
+            return f'LOCALHOST: Registro de localhost inserido em Hosts.JSON.' 
+
+        else:
+            for node in _node_objects_: 
+                if 'localhost' != node.name:
+                    write_host_object = write_host()
+                    write_host_object.set_host( name = self.name,
+                                        ip = self.ip,
+                                        description = self.description,
+                                        uid = '000',
+                                        edge = '')
+                
+                    return f'LOCALHOST: Ja existem dados no arquivo de hosts, registro de localhost inserido em Hosts.JSON.' 
+
+            else:
+
+                return f'LOCALHOST: Registro de localhost ja existe em Hosts.JSON.'
+
 
     def get_ip(self):
         """ Retorna ip da inteface de rede do localhost (nao da loopback)."""
